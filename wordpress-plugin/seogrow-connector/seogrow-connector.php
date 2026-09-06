0aa0e2cbe5bb08c63d2748f77fb7b5a92bee3898
Roberto Zanoni <cod.zanoni@gmail.com>
2026-09-06 08:30:40 +0100

    feat: load taxonomy cache-coherence capability

--- wordpress-plugin/seogrow-connector/seogrow-connector.php
+++ wordpress-plugin/seogrow-connector/seogrow-connector.php
@@ -17,3 +17,4 @@ require_once __DIR__ . '/elementor-reference-read.php';
 require_once __DIR__ . '/wordpress-public-inventory-paged.php';
 require_once __DIR__ . '/taxonomy-diagnostics-read.php';
 require_once __DIR__ . '/taxonomy-persistence-proof.php';
+require_once __DIR__ . '/taxonomy-cache-coherence-capability.php';
