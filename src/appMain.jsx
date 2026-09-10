b1154556fee0cbbfb2c95826bb054353bc28c82a
Roberto Zanoni <cod.zanoni@gmail.com>
2026-09-10 17:05:45 +0100

    Load sidebar readability layer after legacy styles

--- src/appMain.jsx
+++ src/appMain.jsx
@@ -18,7 +18,7 @@ import CorrectionsWorkspace from './CorrectionsWorkspace';
 import './styles.css';
 import './responsiveIntegrity.css';
 import './reflowNavigationFix.css';
-import './ProjectCenterCardFlow.css';
+import './SidebarReadabilityFix.css';
 
 class AppErrorBoundary extends React.Component {
   constructor(props) {
