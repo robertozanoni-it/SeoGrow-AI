c44955f4ae9332ab4dbeebc10394599deafcf501
Roberto Zanoni <cod.zanoni@gmail.com>
2026-09-12 16:54:17 +0100

    Install guided navigation bridge before React layers

--- src/appMain.jsx
+++ src/appMain.jsx
@@ -3,6 +3,7 @@ import './remediationVerificationMigration';
 import './remediationIntegrity';
 import './writeRecovery';
 import './locationEvents';
+import './GuidedNavigationBridge';
 import './PageRouteReconciler';
 import './seoResponseIntegrity';
 import './taskClientIdIntegrity';
