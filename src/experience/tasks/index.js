68fea69f60639260682dc1ef4266a69073aa4d62
Roberto Zanoni <cod.zanoni@gmail.com>
2026-09-15 11:04:21 +0100

    Add Tasks public experience boundary

---
+++ src/experience/tasks/index.js
@@ -0,0 +1,5 @@
+// Public API for the Tasks experience.
+//
+// Cross-module work remains coordinated through the shared Tasks experience;
+// consumers should use this boundary as task behavior is extracted.
+export { tasksManifest } from "./manifest.js";
