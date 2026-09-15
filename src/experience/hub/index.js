2f9760af9936efc8e800ee94df3b2c40976079b9
Roberto Zanoni <cod.zanoni@gmail.com>
2026-09-15 14:23:45 +0100

    Move client-card navigation into Hub ownership (#138)
    * Move client card navigation into Hub
    
    * Keep client card navigation compatibility shim
    
    * Expose client-card navigation from Hub public API
    
    * Guard Hub client-card navigation ownership

--- src/experience/hub/index.js
+++ src/experience/hub/index.js
@@ -3,3 +3,4 @@
 // Hub remains the owner of overview/project experience routes while their
 // implementation is extracted from the legacy shell incrementally.
 export { hubManifest } from "./manifest.js";
+export { clientForCard, selectCardClient } from "./clientCardNavigation.js";
