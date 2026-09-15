ea09f382398c6ed61bfcb6a8879cf7f43e0ddf17
Roberto Zanoni <cod.zanoni@gmail.com>
2026-09-15 12:12:17 +0100

    Move Google integration state logic into System module

--- src/system/index.js
+++ src/system/index.js
@@ -3,3 +3,7 @@
 // Integrations and settings stay behind this boundary while implementation is
 // extracted from the legacy shell without changing persisted configuration.
 export { systemManifest } from "./manifest.js";
+export {
+  mergeGoogleStatus,
+  normalizeGoogleProperties,
+} from "./integrations/googleProperties.js";
