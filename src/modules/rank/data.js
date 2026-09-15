8274dde9721bca8a922589ac8331222a60003d51
Roberto Zanoni <cod.zanoni@gmail.com>
2026-09-15 17:46:13 +0100

    Expose page suggestion from Rank data API

--- src/modules/rank/data.js
+++ src/modules/rank/data.js
@@ -1,6 +1,7 @@
 // Pure data API for Rank & Growth consumers that do not need the full facade.
 // Cross-domain modules should prefer this entrypoint for Rank-owned data behavior.
 export { opportunityQueries } from "./opportunityQueries.js";
+export { suggestPageForQuery } from "./pageSuggestion.js";
 export {
   opportunityGroups,
   queryChanges,
