c10a65583ea8c61b43359418366bb91a91384a30
Roberto Zanoni <cod.zanoni@gmail.com>
2026-09-15 11:04:09 +0100

    Add Links public module boundary

---
+++ src/modules/links/index.js
@@ -0,0 +1,5 @@
+// Public API for the Links domain.
+//
+// Keep this facade stable while internal link and broken-link implementation
+// is moved out of the legacy application incrementally.
+export { linksManifest } from "./manifest.js";
