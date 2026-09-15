934c2ff7dca5328ca4951138ab722ed51d9a662f
Roberto Zanoni <cod.zanoni@gmail.com>
2026-09-15 11:04:17 +0100

    Add Hub public experience boundary

---
+++ src/experience/hub/index.js
@@ -0,0 +1,5 @@
+// Public API for the SeoGrow Hub experience.
+//
+// Hub remains the owner of overview/project experience routes while their
+// implementation is extracted from the legacy shell incrementally.
+export { hubManifest } from "./manifest.js";
