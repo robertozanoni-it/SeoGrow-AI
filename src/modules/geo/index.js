353240de1811f1ab683b7268ee36b2169965af2d
Roberto Zanoni <cod.zanoni@gmail.com>
2026-09-17 17:38:51 +0100

    Expose GEO evidence and operational contract

--- src/modules/geo/index.js
+++ src/modules/geo/index.js
@@ -1,2 +1,8 @@
 export { default as GeoPage } from "../../GeoPage.jsx";
 export { geoManifest } from "./manifest.js";
+export {
+  GEO_SCOPE,
+  buildGeoEvidenceModel,
+  buildGeoOperationalItems,
+  geoOpportunityInputs,
+} from "./geoOperationalModel.js";
