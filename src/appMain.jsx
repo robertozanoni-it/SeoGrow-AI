df45f2f3d9d2bade641f0153dc4f3746f1318c6e
Roberto Zanoni <cod.zanoni@gmail.com>
2026-09-17 11:42:07 +0100

    Remove global audit evidence side effect

--- src/appMain.jsx
+++ src/appMain.jsx
@@ -6,7 +6,6 @@ import './locationEvents';
 import './GuidedNavigationBridge';
 import './PageRouteReconciler';
 import './seoResponseIntegrity';
-import './AuditEvidenceUx';
 import './taskClientIdIntegrity';
 import './uiIntegrityFixes';
 import './WizardStepNavigation';
