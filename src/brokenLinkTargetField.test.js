e524d4d3ca41dce83d4e2a5e6ca88ff87899bde5
Roberto Zanoni <cod.zanoni@gmail.com>
2026-09-18 02:31:33 +0100

    Align Connector 1.3.9 test expectations

--- src/brokenLinkTargetField.test.js
+++ src/brokenLinkTargetField.test.js
@@ -18,9 +18,9 @@ test("il link esterno 404 è mostrato in un campo dedicato con Apri e Copia", ()
 });
 
 test("il pacchetto Connector aggiornato è distinguibile come 1.3.9", () => {
-  assert.match(connector, /Version:\s*1\.3\.8/);
+  assert.match(connector, /Version:\s*1\.3\.9/);
   assert.match(connector, /elementor-link-cleanup-write\.php/);
   assert.match(connector, /elementor-shared-link-remediation\.php/);
-  assert.match(workflow, /seogrow-connector-1\.3\.8/);
-  assert.match(workflow, /artifacts\/seogrow-connector-1\.3\.8\.zip/);
+  assert.match(workflow, /seogrow-connector-1\.3\.9/);
+  assert.match(workflow, /artifacts\/seogrow-connector-1\.3\.9\.zip/);
 });
