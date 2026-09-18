005d961c6d45eddc780f21759bc736b6ccd2a7ab
Roberto Zanoni <cod.zanoni@gmail.com>
2026-09-18 02:28:17 +0100

    Update Connector package version assertion

--- src/brokenLinkTargetField.test.js
+++ src/brokenLinkTargetField.test.js
@@ -17,7 +17,7 @@ test("il link esterno 404 è mostrato in un campo dedicato con Apri e Copia", ()
   assert.match(css, /\.seogrow-broken-link-target-row/);
 });
 
-test("il pacchetto Connector aggiornato è distinguibile come 1.3.8", () => {
+test("il pacchetto Connector aggiornato è distinguibile come 1.3.9", () => {
   assert.match(connector, /Version:\s*1\.3\.8/);
   assert.match(connector, /elementor-link-cleanup-write\.php/);
   assert.match(connector, /elementor-shared-link-remediation\.php/);
